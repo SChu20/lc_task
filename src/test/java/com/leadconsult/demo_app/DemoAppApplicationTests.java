cde28a4ad0fd74955e21a38771a9a3c7391cfde8
Slav Chushev <chushev.slav@gmail.com>
2025-05-12 11:48:14 +0300

    Initial commit

---
+++ src/test/java/com/leadconsult/demo_app/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadconsult.demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
